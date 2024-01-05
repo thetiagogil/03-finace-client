ca13b8cb50984b71cfb97457125f1941e2f0ca73
thetiagogil <talmeidagil@gmail.com>
2024-01-05 20:50:20 +0000

    react strict mode unhidden

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,11 @@ import { BrowserRouter } from "react-router-dom";
 import AuthContextProvider from "./contexts/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
-  </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
